5216ab0d59062df4cd2e90917667a7939965871c
badboy <414972754@qq.com>
2018-04-04 21:22:35 +0800

    初始化

---
+++ 01/HelloWorld.java
@@ -0,0 +1,21 @@
+package com;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+   /* 注释的作用：
+    	A：解释说明程序，提高程序的阅读性
+    	B:可以帮助我们调视程序
+	*/
+	
+	//关键字：被java语言赋予特定含义的单词
+	//关键字的特点：关键字都是小写的
+	//关键字注意事项
+	//A：goto和const作为保留字存在，目前并不使用
+	public static void main(String[] args) {
+		System.out.println("你可以放弃选择，但是不能选择放弃！");
+	}
+
+}
